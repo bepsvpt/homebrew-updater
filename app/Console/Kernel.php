932474a4800f3580fe66f1cd08bf0a851f626a2b
BePsvPT <freedom@csie.io>
2016-12-10 01:17:27 +0800

    Modify schedule frequency

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('formula:check')->twiceDaily();
+         $schedule->command('formula:check')->hourly();
     }
 
     /**
