af2388ca466bdcdc8c2a5090123e48a89a9b8a71
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2019-12-29 17:36:04 +0800

    migrate to laravel upstream

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,17 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| Here you may define all of your model factories. Model factories give
-| you a convenient way to create models for testing and seeding your
-| database. Just tell the factory how a default model should look.
-|
-*/
+use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory;
 
-$factory->define(App\Models\Formula::class, function (Faker\Generator $faker) {
+/** @var Factory $factory */
+
+$factory->define(App\Models\Formula::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'url' => $faker->url,
